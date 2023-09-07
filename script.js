6d87a31dfb41eacbf69bea68955cfa1b21a1d8ea
Rajibul Islam <coolrajib32@gmail.com>
2023-09-07 10:30:36 +0530

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let h2 = document.getElementById('h2');
-let button = document.getElementById('button1')
-function friend(){
+let button = document.getElementById('button1');
+let check = 0 ;
+
+/*function friend(){
    if(button.innerText =='Add Friend'){
     h2.innerText = 'Friends';
     h2.style.color = '#00ff88';
@@ -14,4 +16,21 @@ function friend(){
     button.style.backgroundColor='#00ff88';
     button.style.color='black'
    }
+}*/
+function friend (){
+   if(check == 0 ){
+   h2.innerText = 'Friends';
+    h2.style.color = '#00ff88';
+    button.innerText = 'Remove Friend';
+    button.style.backgroundColor='Red';
+    button.style.color='white';
+    check = 1
+}else{
+   h2.innerText = 'Stranger';
+    h2.style.color = 'Red';
+    button.innerText = 'Add Friend';
+    button.style.backgroundColor='#00ff88';
+    button.style.color='black';
+    check = 0
+}
 }
